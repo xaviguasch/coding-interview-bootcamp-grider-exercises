0a975d3bf20a4f061af2b45701a7d63086979323
xaviguasch <xaviguasch@gmail.com>
2023-03-05 15:55:30 +0100

    added solution for s8-array-chunking challenge

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,22 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunked = []
 
-module.exports = chunk;
+  for (const el of array) {
+    const last = chunked[chunked.length - 1]
+
+    if (!last || last.length === size) {
+      chunked.push([el])
+    } else {
+      last.push(el)
+    }
+  }
+
+  return chunked
+}
+
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3))
+
+module.exports = chunk
